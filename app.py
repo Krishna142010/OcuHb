f0ac23c644f3e4edd8aa1c5641243943a8cfb814
Krishna142010 <bansalkrishna788@gmail.com>
2026-03-01 16:33:53 +0530

    Update model loading and application title

--- app.py
+++ app.py
@@ -4,17 +4,20 @@ import tensorflow as tf
 from PIL import Image
 import cv2
 
-# Load model
-model = tf.keras.models.load_model("saved_models/best_EfficientNetB0.keras")
+# Load model safely
+model = tf.keras.models.load_model(
+    "saved_models/best_EfficientNetB0.keras",
+    compile=False
+)
 
-st.title("ConjunctivaScan 👁️")
+st.title("OcuHb 👁️")
 st.write("AI-based anemia screening (NOT diagnostic)")
 
 uploaded_file = st.file_uploader("Upload an eye image", type=["jpg", "png"])
 
 if uploaded_file:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image")
 
     img = np.array(image)
     img = cv2.resize(img, (224, 224)) / 255.0
